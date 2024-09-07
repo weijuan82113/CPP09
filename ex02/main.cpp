fae092fd4392ebe67420711a430b7dff8639abe9
Weijuan Chen <wchen@c5r1s4.42tokyo.jp>
2024-09-07 18:10:06 +0900

    UPDATE ex01 ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char* argv[])
 {
 	if (argc < 2)
+	{
 		std::cerr << "No positive integer sequence be inputed" << std::endl;
+		return 1;
+	}
 	try
 	{
 		PmergeMe p;
@@ -19,6 +22,7 @@ int main(int argc, char* argv[])
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
+		return 1;
 	}
 	return 0;
 }
